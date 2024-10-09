6c514a189c7662d430dc5e188e7ff6d6ae7e5788
roberto.morano <roberto.morano@106-23.info2>
2024-10-09 11:07:34 +0200

    1B3

--- Tema2/src/boletin3/Ej1.java
+++ Tema2/src/boletin3/Ej1.java
@@ -1,9 +1,40 @@
 package boletin3;
 
+import java.util.Scanner;
+
 public class Ej1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1;
+		//variable con la que operamos
+		int n;
+		Scanner sc = new Scanner(System.in);
+		// Preguntamos
+		System.out.println("Dame un numero");
+		// Leer el numero introducido por pantalla
+		num1 = sc.nextInt();
+		if ((num1>=0)&&(num1<10000)) {
+			if (num1 < 10) {
+				System.out.println("Es capicua pues solo tiene una cifra");
+			}else if(num1 < 100){
+				n = num1%10;
+				if (n == (num1/10)) {
+					System.out.println("Es capicua");}
+				}else if(num1 <1000) {
+					n = num1%10;
+					if (n == (num1/100)) {
+						System.out.println("Es capicua");};
+				}else if(num1<10000) {
+					n = num1%10;
+					if (n == (num1/1000)) {
+						n = (((num1%1000)%100)%10);
+						if (n == num1/100);
+						}
+				}
+		}else{
+			System.out.println("Numero fuera de rango");
+		}
+		
 
 	}
 
